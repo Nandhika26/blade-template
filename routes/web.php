8ce4a78095f095d7bc90e93be2254fb3093b5a1c
Nandhika26 <nandikadiega@gmail.com>
2024-05-14 15:23:20 +0700

    Blade Templating

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/widget', function () {
+    return view('widget');
+});
